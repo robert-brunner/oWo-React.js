439dbcfb7a23ef4b1574835e0a9e5777a92b73df
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2023-07-14 18:22:12 -0400

    added SurfMenu.js

--- src/App.js
+++ src/App.js
@@ -22,30 +22,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-// import React from 'react';
-// import { Helmet } from 'react-helmet';
-// import Beach from './components/nav/Surf.js';
-
-// function App() {
-//   return (
-//     <>
-//       <Helmet>
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-//         <meta name="theme-color" content="#000000" />
-//         <meta name="apple-mobile-web-app-capable" content="yes" />
-//         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-//       </Helmet> {/**Prevents the window from resizing when you pinch it with your fingers.  Very annoying. */}
-//       <div style={{ flex: 1 }}>
-//         <Beach />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Beach from '../nav/Surf';
+import BeachMenu from '../nav/SurfMenu';
 
 export default function ApplicationViews() {
   return (
     <Routes>
       <Route path="/SurfGame" element={<Beach />} />
+      <Route path="/SurfMenu" element={<BeachMenu />} />
     </Routes>
   );
 }
